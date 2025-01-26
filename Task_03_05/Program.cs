0cb252c719622d79fd2208563266902a0ad4f8a7
sviridovda <sviridovdenis156@gmail.com>
2025-01-26 17:42:28 +0500

    выполнено задание 5 с 3 урока

---
+++ Task_03_05/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_03_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите начальную температуру в градусах Цельсия: ");
+            double startCelsius = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите конечную температуру в градусах Цельсия: ");
+            double endCelsius = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите шаг изменения температуры в градусах Цельсия: ");
+            double step = Convert.ToDouble(Console.ReadLine());
+
+            // Вывод заголовка таблицы
+            Console.WriteLine("\nТемпература (°C) | Температура (°F)");
+            Console.WriteLine("-------------------|-------------------");
+
+            // Цикл для вычисления и вывода значений
+            for (double celsius = startCelsius; celsius <= endCelsius; celsius += step)
+            {
+                double fahrenheit = celsius * 1.8 + 32;
+                Console.WriteLine($"{celsius,-18:F2} | {fahrenheit,-18:F2}");
+            }
+        }
+    }
+}
+
