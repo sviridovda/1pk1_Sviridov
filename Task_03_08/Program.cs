7271a57f4a77b2639b5b209b3feeac561352268a
sviridovda <sviridovdenis156@gmail.com>
2025-01-26 17:52:26 +0500

    выполнено задание 8 с 3 урока

---
+++ Task_03_08/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task_03_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Перебираем числа от 20 до 50
+            for (int i = 20; i <= 50; i++)
+            {
+                // Проверяем, делится ли число на 3 и не делится ли на 5
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine(i); // Выводим число
+                }
+            }
+        }
+    }
+}
+    
+
