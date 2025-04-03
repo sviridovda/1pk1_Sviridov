336bc07303028dbc3d18655ac4822bed7c4ed688
Denis Sviridov <sviridovdenis125@gmail.com>
2025-04-03 19:03:37 +0300

    Выполнено задание 4

--- Task_20_4/Program.cs
+++ Task_20_4/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace Task_20_4
+﻿namespace Task_20_04
 {
     internal class Program
     {
+        /*
+         * Создайте программу для учета транспортных средств. Используйте перечисление VehicleType:
+            • Car
+            • Bike
+            • Bus
+            • Truck
+            • Motorcycle
+            Храните список транспортных средств (можно просто List<VehicleType>). 
+        Добавьте метод для подсчёта транспорта определённого типа (например, сколько грузовиков). 
+        Реализуйте поиск по типу и вывод информации.
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            VehicleType truck1 = new VehicleType(Vehicle.Truck);
+            truck1.ShowInfo();
+
+            VehicleType truck2 = new VehicleType(Vehicle.Truck);
+            VehicleType truck3 = new VehicleType(Vehicle.Truck);
+            truck2.ShowInfo();
+
+            VehicleType car1 = new VehicleType(Vehicle.Car);
+            car1.ShowInfo();
+
+            VehicleType bus1 = new VehicleType(Vehicle.Bus);
+            VehicleType bus2 = new VehicleType(Vehicle.Bus);
+            VehicleType bus3 = new VehicleType(Vehicle.Bus);
+            VehicleType bus4 = new VehicleType(Vehicle.Bus);
+            VehicleType bus5 = new VehicleType(Vehicle.Bus);
+            bus1.ShowInfo();
         }
     }
 }
---
+++ Task_20_4/VehicleType.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20_04
+{
+    enum Vehicle
+    {
+        Car,
+        Bike,
+        Bus,
+        Truck,
+        Motorcycle
+    }
+
+    internal class VehicleType
+    {
+        /*
+         * Создайте программу для учета транспортных средств. Используйте перечисление VehicleType:
+            • Car
+            • Bike
+            • Bus
+            • Truck
+            • Motorcycle
+            Храните список транспортных средств (можно просто List<VehicleType>). 
+        Добавьте метод для подсчёта транспорта определённого типа (например, сколько грузовиков). 
+        Реализуйте поиск по типу и вывод информации.
+         */
+
+        Vehicle vehicle;
+        static List<Vehicle> vehicleType = new List<Vehicle>();
+        static List<int> vehicleTypeCount = new List<int>() { 0, 0, 0, 0, 0 };
+
+        /// <summary>
+        /// добавление транспорта 
+        /// увеличение кол-во его типа
+        /// и инициализация поля типом транспорта
+        /// </summary>
+        /// <param name="vehicle"></param>
+        public VehicleType(Vehicle vehicle)
+        {
+            vehicleType.Add(vehicle);
+            vehicleTypeCount[(int)vehicle]++;
+            this.vehicle = vehicle;
+        }
+
+        public void ShowInfo()
+        {
+            int countType = vehicleTypeCount[(int)vehicle]; // количество штук типа
+
+            // выбор нужной формs слова штука
+            string piece;
+            switch (countType)
+            {
+                case >= 5:
+                    piece = "штук";
+                    break;
+                case >= 2:
+                    piece = "штуки";
+                    break;
+                default:
+                    piece = "штука";
+                    break;
+            }
+
+            Console.WriteLine($"{vehicle} сейчас в наличии {countType} {piece}");
+        }
+
+    }
+}
