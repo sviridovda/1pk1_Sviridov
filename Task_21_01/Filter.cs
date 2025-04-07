65285c0a9ebb3169efa5776458cae65f300ff8eb
Denis Sviridov <sviridovdenis125@gmail.com>
2025-04-07 20:29:41 +0300

    Выполнена задача 1 с 21 урока

---
+++ Task_21_01/Filter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_21_01
+{
+    static class Filter
+    {
+        /// <summary>
+        /// фильтрует список, удаляя нечётные числа
+        /// и умножая чётные на 10
+        /// </summary>
+        /// <param name="lst"> список </param>
+        public static void FilterList(List<int> lst)
+        {
+            for (int i = 0; i < lst.Count;)
+            {
+                if (lst[i] % 2 != 0) lst.RemoveAt(i);
+                else
+                {
+                    lst[i] *= 10;
+                    i++;
+                }
+            }
+        }
+    }
+}
---
+++ Task_21_01/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Task_21_01
+{
+    internal class Program
+    {
+        /*
+         * Дан список чисел. 
+         * Создать новый список, содержащий только чётные числа из исходного списка, умноженные на 10.
+         */
+        static void Main(string[] args)
+        {
+            List<int> list = new(); // список чисел
+
+            Console.WriteLine("Введите последовательность оканчивающуюся 0:");
+
+            // запрос с консоли последовательности и запись её в список
+            while (true)
+            {
+                int n;
+                if (!int.TryParse(Console.ReadLine(), out n) || n == 0)
+                {
+                    Console.Clear();
+                    break;
+                }
+                list.Add(n);
+            }
+
+            // вывод исходного списка
+            Console.WriteLine("Исходный список:");
+            foreach (int el in list)
+                Console.Write($"{el} ");
+
+            Filter.FilterList(list);
+
+            // вывод отфильтрованнок списка
+            Console.WriteLine("\nОтфильтрованный список:");
+            foreach (int el in list)
+                Console.Write($"{el} ");
+        }
+    }
+}
