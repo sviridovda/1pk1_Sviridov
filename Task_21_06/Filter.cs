092e62fcca8742f67d1d47465406ff6727e6b15d
Denis Sviridov <sviridovdenis125@gmail.com>
2025-04-07 20:43:14 +0300

    Выполнена задача 6 с 21 урока

---
+++ Task_21_06/Filter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_21_06
+{
+    static class Filter
+    {
+        /// <summary>
+        /// удаляет повторяющиеся элементы, сохраняя их порядок
+        /// </summary>
+        /// <param name="list"> список </param>
+        public static void FilterList(List<int> list)
+        {
+            HashSet<int> set = new(list);
+            List<int> newList = new(set);
+
+            for (int i = 0; i < list.Count;)
+            {
+                if (i >= newList.Count) list.RemoveAt(i);
+                else
+                {
+                    list[i] = newList[i];
+                    i++;
+                }
+            }
+
+        }
+    }
+}
---
+++ Task_21_06/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task_21_06
+{
+    internal class Program
+    {
+        /*
+         * Написать метод, который удаляет повторяющиеся элементы из списка, сохраняя порядок.
+         */
+        static void Main(string[] args)
+        {
+            List<int> list = new();
+
+            Console.WriteLine("Введите последовательность оканчивающуюся 0:");
+
+            // ввод с консоли и запись в список чисел
+            while (true)
+            {
+                int elem;
+                int.TryParse(Console.ReadLine(), out elem);
+                if (elem == 0) break;
+                list.Add(elem);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Старый список:");
+            foreach (int el in list)
+                Console.Write($"{el} ");
+
+            Filter.FilterList(list);
+
+            Console.WriteLine("\nОбновлённый список:");
+            foreach (int el in list)
+                Console.Write($"{el} ");
+        }
+    }
+}
