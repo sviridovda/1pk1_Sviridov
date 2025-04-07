0e78512db56340864636cd964e5269f6681751c4
Denis Sviridov <sviridovdenis125@gmail.com>
2025-04-07 20:33:16 +0300

    Выполнена задача 2 с 21 урока

---
+++ Task_21_02/CountWords.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_21_02
+{
+    static class CountWords
+    {
+        /// <summary>
+        /// разбивает текст на слова
+        /// создаёт словарь с ключом строкой и целочисленным значением
+        /// делает подсчёт вхождений слов в тексте
+        /// </summary>
+        /// <param name="text"> текст </param>
+        /// <returns></returns>
+        public static Dictionary<string, int> CountOfWords(string text)
+        {
+            Dictionary<string, int> dict = new();
+
+            // разбитый на слова текст
+            string[] textSplit = text.Split('.', ',', ':', ';', '?', '!', '\'', '"', '(', ')', '%', '$', ' ');
+            for (int i = 0; i < textSplit.Length; i++)
+            {
+                string strKey = textSplit[i];                       // слово
+                if (!dict.ContainsKey(strKey)) dict[strKey] = 0;    // создание новой пары ключа значения
+
+                dict[strKey]++;
+            }
+
+            return dict;
+        }
+    }
+}
---
+++ Task_21_02/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_21_02
+{
+    internal class Program
+    {
+        /*
+         * Дан текст. 
+         * Написать метод, который возвращает словарь, где ключ — слово, а значение —количество его вхождений в тексте.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст:");
+            string text = Console.ReadLine();
+
+            // словарь со словами и кол-ом их вхождений в тексте
+            Dictionary<string, int> dict = CountWords.CountOfWords(text);
+
+            // вывод словаря
+            foreach (var (k, v) in dict)
+            {
+                int len = k.Length;
+                string space = "";
+                for (int i = 0; i < 15 - len; i++) space += " ";
+
+                Console.WriteLine($"Слово: \"{k}\";" + space + $"Кол-во вхождений: {v}.");
+            }
+        }
+    }
+}
