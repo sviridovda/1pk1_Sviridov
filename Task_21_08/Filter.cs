cfb1d4a769e85374e313dcfedf6ca90b33048769
Denis Sviridov <sviridovdenis125@gmail.com>
2025-09-10 19:53:38 +0500

    выполнена задача 8 с 21 урока

---
+++ Task_21_08/Filter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_21_08
+{
+    /*
+         * напишите метод, который на вход получается массив параметров (строк) и возвращает только уникальные строки
+         */
+    static class Filter
+    {
+        public static string[] FilterStrings(params string[] strs)
+        {
+            HashSet<string> set = new();
+            string[] newStrs = new string[100];
+
+            foreach (string str in strs)
+                set.Add(str);
+
+            int i = 0;
+            while (set.Count != 0)
+            {
+                newStrs[i++] = set.First();
+                set.Remove(set.First());
+            }
+
+            return newStrs;
+        }
+    }
+}
---
+++ Task_21_08/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_21_08
+{
+    internal class Program
+    {
+        /*
+         * напишите метод, который на вход получается массив параметров (строк) и возвращает только уникальные строки
+         */
+        static void Main(string[] args)
+        {
+            string[] strs = new string[100];
+
+            Console.WriteLine("Введите последовательность строк (не более 100), оканчивающуюся пустой строкой:");
+
+            int i = 0;
+            while (true)
+            {
+                string str = Console.ReadLine();
+                if (str == "" || i >= strs.Length) break;
+                strs[i++] = str;
+            }
+
+            string[] newStrs = Filter.FilterStrings(strs);
+
+            Console.WriteLine("Обновлённая последовательность строк:");
+            foreach (string str in newStrs)
+                if (str != null) Console.WriteLine(str);
+        }
+    }
+}
