201baf87c8ca5e74c076113c13db3b6fbdac6275
Denis Sviridov <sviridovdenis125@gmail.com>
2025-09-10 19:20:46 +0500

    Выполнена задача 1 к 11 уроку

---
+++ Task_11_01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task_11_01
+{
+    internal class Program
+    {
+        /*Передача по значению: Напишите метод, который принимает два целых числа и меняет их
+        местами. Проверьте, изменились ли значения переменных вне метода
+         */
+        static void Main(string[] args)
+        {
+            int num1 = 2;
+            int num2 = 7;
+            Console.WriteLine($"Числа: {num1}, {num2}");
+            Reverse(num1, num2);
+            Console.WriteLine($"Числа после замены местами: {num1}, {num2}");
+        }
+
+        /// <summary>
+        /// Меняет числа местами
+        /// </summary>
+        /// <param name="a">первое число</param>
+        /// <param name="b">второе число</param>
+
+        static void Reverse(int a, int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
