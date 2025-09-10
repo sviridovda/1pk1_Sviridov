b23f025db1e07c2735c069ac9cf40b61859b677b
Denis Sviridov <sviridovdenis125@gmail.com>
2025-09-10 20:01:16 +0500

    выполнена задача 2 с 23 урока

---
+++ Task_23_02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task_23_02
+{
+    internal class Program
+    {
+        /*осуществите полный анализ каталогов в произвольной папке:
+        название
+        полный путь
+        дата создания
+        корневой каталог
+         */
+        static void Main(string[] args)
+        {
+            Console.Write("Введите путь к существующему каталогу: ");
+            string path = Console.ReadLine();
+            if (Directory.Exists(path))
+            {
+                var directory = new DirectoryInfo(path);
+                Console.WriteLine($"название: {directory.Name}\nполный путь: {directory.FullName}\n" +
+                    $"дата создания: {directory.CreationTime}\nкорневой каталог: {directory.Root}");
+            }
+            else
+            {
+                Console.WriteLine("НЕ существует введенного вами каталога!");
+            }
+        }
+    }
+}
