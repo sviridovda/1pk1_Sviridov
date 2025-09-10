b6a799d07a7b035c24497156b3217db4165bab5e
Denis Sviridov <sviridovdenis125@gmail.com>
2025-09-10 18:53:08 +0500

    Выполнена задача 9 с 4 урока

---
+++ Task_04_09/Program.cs
@@ -0,0 +1,44 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+namespace Task_04_09
+{
+    /*В массиве найти элементы,
+     * которые в нем встречаются только один раз,
+     * и вывести их на экран. 
+     * То есть найти и вывести уникальные элементы массива.
+     * (LINQ использовать нельзя)
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //введём данные для массива
+            Console.Write("Введите количество значений в массиве: ");
+            int massiv = int.Parse(Console.ReadLine());
+            Console.Write("Введите начальное значение в массиве: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.Write("Введите конечное значение в массиве: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] ints = new int[massiv];
+            Random rnd = new Random();
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                ints[i] = rnd.Next(m, n);
+            }
+            //выведем уникальные элементы массива
+            Console.WriteLine("Уникальные элементы массива: ");
+            for (int i = 0; i < ints.Length; i++)
+            {
+                int numbers = 0;
+                for (int j = 0; j < ints.Length; j++)
+                {
+                    if (ints[i] == ints[j])
+                        numbers++;
+                }
+                if (numbers == 1) Console.Write(ints[i] + " ");
+            }
+        }
+    }
+}
