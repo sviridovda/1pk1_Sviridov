b5cb3f672c282d5d20a1fd56b4bcbda865dbe667
Denis Sviridov <sviridovdenis125@gmail.com>
2025-09-10 20:02:08 +0500

    выполнена задача 3 с 23 урока

---
+++ Task_23_03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml.Linq;
+
+namespace Task_23_03
+{
+    internal class Program
+    {
+        /*в произвольном каталоге удалите все вложенные каталоги с игнорированием наличия содержимого
+         */
+        static void Main(string[] args)
+        {
+            Console.Write("Введите путь к существующему каталогу: ");
+            string path = Console.ReadLine();
+            DirectoryInfo rootDirectory = new DirectoryInfo(path);
+            if (rootDirectory.Exists)
+            {
+                List<DirectoryInfo> directories = rootDirectory.GetDirectories().ToList();
+                foreach (DirectoryInfo d in directories)
+                {
+                    d.Delete(true);
+                }
+            }
+            else
+            {
+                Console.WriteLine("НЕ существует введенного вами каталога!");
+            }
+        }
+    }
+}
