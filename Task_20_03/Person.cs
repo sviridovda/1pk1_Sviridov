5c6e414f6abfadb1c3d1237fad621c58bfe80256
Denis Sviridov <sviridovdenis125@gmail.com>
2025-09-10 19:50:05 +0500

    Выполнена задача 3 с 20 урока

---
+++ Task_20_03/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_20_03
+{
+    internal class Person
+    {
+        public OrderStatus Status { get; set; }
+        public void Change()
+        {
+            if (Status == OrderStatus.Delivered || Status == OrderStatus.Cancelled)
+                Console.WriteLine($"Нельзя изменить статус заказа, так как {Status}");
+            else
+            {
+                Status++;
+                Console.WriteLine($"Заказ переведён в статус: {Status}");
+            }
+        }
+        public void Cancel()
+        {
+            if (Status == OrderStatus.Delivered || Status == OrderStatus.Cancelled)
+                Console.WriteLine($"Нельзя изменить статус заказа, так как {Status}");
+            else
+            {
+                Status = OrderStatus.Cancelled;
+                Console.WriteLine($"Заказ переведён в статус: {Status}");
+            }
+        }
+        public Person()
+        {
+            Status = OrderStatus.New;
+        }
+    }
+}
---
+++ Task_20_03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Task_20_03
+{
+    enum OrderStatus
+    {
+        New,
+        Processing,
+        Shipped,
+        Delivered,
+        Cancelled
+    }
+    internal class Program
+    {
+        /*Создайте программу для управления статусом заказа в интернет-магазине. 
+         * Используйте перечислениеOrderStatus со значениями:
+         * • New (новый)
+         * • Processing (в обработке)
+         * • Shipped (отправлен)
+         * • Delivered (доставлен)
+         * • Cancelled (отменён)
+         * Напишите метод, который меняет статус заказа. 
+         * Если заказ уже доставлен или отменён, запретите изменение статуса. 
+         * Выводите сообщение при каждом изменении статуса
+         * (например, "Заказ переведён в статус: Отправлен")
+         */
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.WriteLine(person.Status);
+            person.Change();
+            person.Change();
+            person.Cancel();
+            person.Change();
+        }
+
+    }
+}
