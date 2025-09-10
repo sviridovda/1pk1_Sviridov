90a8bd5217376229fe9e7196768be0551bc0416e
Denis Sviridov <sviridovdenis125@gmail.com>
2025-09-10 19:43:40 +0500

    выполнена задача 3 с 13 урока

---
+++ Task_13_03/Car.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_13_03
+{
+    /// <summary>
+    /// класс описывающий студента с характеристиками: имя, фамилия, отчество, дата рождения
+    /// </summary>
+    internal class Car
+    {
+        #region поля
+        private string name;        //закрытое поле имени
+        private string surname;     //закрытое поле фамилии
+        private string patronomyc;  //закрытое поле отчества
+        private DateTime birthday;  //закрытое поле даты рождения
+        #endregion
+
+        #region свойства
+        //открытое свойство имени, связанное с полем имени и выполняющее предварительную проверка на попытку записать пустое имя
+        public string Name
+        {
+            get { return name; } //аксессор для чтения значения поля
+            set                  //аксессор для записи значения в поле
+            {
+                if (value != null) //если приходящее значение не null
+                    name = value;   //то в поле записывается приходящее значение
+                else Console.WriteLine("Warning! name is empty"); //иначе выводится сообщение о некорректных вх данных и запись в поле не происходит
+            }
+        }
+        //открытое свойство фамилии (логика совпадает с именем)
+        public string Surname
+        {
+            get { return surname; }
+            set
+            {
+                if (value != null)
+                    surname = value;
+                else Console.WriteLine("Warning! surname is empty");
+            }
+        }
+        //открытое свойство отчетства (логика совпадает с именем)
+        public string Patronomyc
+        {
+            get { return patronomyc; }
+            set
+            {
+                if (value != null)
+                    patronomyc = value;
+                else Console.WriteLine("Warning! patronomyc is empty");
+            }
+        }
+        //открытое свойство даты рождения
+        public DateTime Birthday
+        {
+            get { return birthday; }
+            set
+            {
+                birthday = value;
+            }
+        }
+        #endregion
+
+        #region методы
+        /// <summary>
+        /// возврат текущей информации по объекту
+        /// </summary>
+        /// <returns>строка с информацией</returns>
+        public string GetInfo()
+        {
+            return $"{surname} {name} {patronomyc}\n" +
+                $"дата рождения: {birthday.ToShortDateString()}";
+        }
+        #endregion
+
+        #region конструкторы
+        /*можно создавать неограниченное количество конструкторов 
+         * для создания объектов с разным набором параметров*/
+
+        /// <summary>
+        /// конструктор создает объект студента с именем и фамилией, остальные
+        /// поля остаются непроинициализированы
+        /// </summary>
+        /// <param name="name">имя</param>
+        /// <param name="surname">фамилия</param>
+
+        public Car(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+        /// <summary>
+        /// конструктор создает объект студента с именем, фамилией и отчетством
+        /// дата рождения остается не заданной
+        /// </summary>
+        /// <param name="name">имя</param>
+        /// <param name="surname">фамилия</param>
+        /// <param name="patronomyc">отчетсво</param>
+        public Car(string name, string surname, string patronomyc)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.patronomyc = patronomyc;
+        }
+        /// <summary>
+        /// полный конструктор, с помощью которого заполняются данными все поля
+        /// </summary>
+        /// <param name="name">имя</param>
+        /// <param name="surname">фамилия</param>
+        /// <param name="patronomyc">отчетство</param>
+        /// <param name="bd">дата рождения</param>
+        public Car(string name, string surname, string patronomyc, DateTime bd)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.patronomyc = patronomyc;
+            this.birthday = bd;
+        }
+        /// <summary>
+        /// конструтор по умолчанию, который становиться недоступным как только прописываются
+        /// остальные конструкторы и его необходимо явно прописать
+        /// </summary>
+        public Car() { }
+
+        #endregion
+    }
+}
---
+++ Task_13_03/Program.cs
@@ -0,0 +1,25 @@
+﻿using static Task_13_03.Car;
+
+namespace Task_13_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student student = new Student("Иван", "Иванов");
+            //Console.WriteLine(student.GetInfo());
+
+
+            //Student student2 = new Student("Сергей", "Петров", "Иванович", new DateTime(2005, 10, 05));
+            //Console.WriteLine(student2.GetInfo());
+
+            Car st = new Car();
+            st.Name = "Петр";
+            st.Surname = "Иванов";
+            st.Patronomyc = null;
+            st.Birthday = DateTime.Now;
+
+            Console.WriteLine(st.GetInfo());
+        }
+    }
+}
